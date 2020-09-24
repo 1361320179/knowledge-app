aeb0651135b3d18937e5c08cb6f58dc1f1520f98
zhuboyuC <897419721@qq.com>
2020-09-24 15:13:28 +0800

    zhuboyuC

--- src/components/easyNav.vue
+++ src/components/easyNav.vue
@@ -114,6 +114,8 @@
         <div>我的</div>
       </router-link>
     </div>
+    <div style="word-break: break-word;">{{ ceshi1 }}</div>
+    <div style="word-break: break-word;">{{ ceshi2 }}</div>
   </div>
 </template>
 
@@ -203,9 +205,13 @@ export default {
         goods_nums: 0,
       },
       is_Login: null,
+      ceshi1: '',
+      ceshi2: '',
     };
   },
   mounted() {
+    this.ceshi1 = localStorage.getItem("ceshi222");
+    this.ceshi2 = localStorage.getItem("ceshi333");
     this.home_id = localStorage.getItem("home_id");
     if (this.type === undefined) {
       this.type = this.navData.type;
